13ae955d632bf4ed6543190e3f9998748dcf3c51
Sofia <sofiataipa222@gmail.com>
2020-12-08 20:11:15 +0000

    Create ServidorRobot.java

---
+++ src/tp2/ServidorRobot.java
@@ -0,0 +1,16 @@
+package TP2;
+
+public class ServidorRobot {
+
+	GUIServidor gui;
+	
+	BufferCircular buffer;
+	RobotDesenhador robot;
+	
+	public ServidorRobot(BufferCircular buffer, RobotDesenhador robot) {
+		this.gui = new GUIServidor();
+		this.buffer = buffer;
+		this.robot = robot;
+	}
+
+}
