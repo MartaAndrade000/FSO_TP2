5a0983babc51c0ba1280b1d417cd492a222a98c9
Sofia <sofiataipa222@gmail.com>
2020-12-08 20:12:34 +0000

    Create IBufferCircularGUI.java

---
+++ src/tp2/demo buffer/IBufferCircularGUI.java
@@ -0,0 +1,10 @@
+package tp2;
+
+public interface IBufferCircularGUI {
+
+    public void setIndexPut(int pos, String setText);
+    public void setIndexPut(int pos);
+    public void setIndexGet(int pos);
+    public void displayNumberOfItems(int numItens);
+    public void dispose();
+}
