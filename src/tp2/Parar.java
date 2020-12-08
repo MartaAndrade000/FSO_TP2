670278c6d9092d1627034c7bb73726b6a0a458c2
Sofia <sofiataipa222@gmail.com>
2020-12-08 20:11:21 +0000

    Create Parar.java

---
+++ src/tp2/Parar.java
@@ -0,0 +1,11 @@
+package TP2;
+
+public class Parar extends Mensagem {
+
+	boolean assincrono;
+	public Parar(boolean assincrono) {
+		super(Mensagem.PARAR, String.format("Parar (%s) \n \n", assincrono));
+		this.assincrono = assincrono;
+	}
+
+}
